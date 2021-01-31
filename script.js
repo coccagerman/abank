a4069759058656952c06efd9065c904d70118528
Germán <coccagerman@gmail.com>
2021-01-31 12:08:49 -0300

    1st commit.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// ----------------------- CONSTRUCCION DE OBJETO -----------------------
+
+function Credito (tipo, monto, plazo) {
+    this.tipo = tipo;
+    this.monto = monto;
+    this.plazo = plazo;
+
+    this.simulacion = function () {
+    return monto / plazo; 
+    }
+}
+
+// ----------------------- CREACION DE OBJETO -----------------------
+
+var miCredito = new Credito (prompt ("Qué tipo de crédito quiere solicitar?"), parseInt (prompt ("Por qué monto le gustaría solicitar el crédito?")), parseInt (prompt ("En cuántas cuotas le gustaría devolverlo?")));
+
+// ----------------------- UTILIZACION DE METODO -----------------------
+
+alert ("Para obtener un crédito " + miCredito.tipo + " por " + miCredito.monto + " con las características que usted requirió, deberá abonar " + miCredito.plazo + " cuotas mensuales de " + miCredito.simulacion() + ".")
