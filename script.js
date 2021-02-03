89e3cf2dfa24837b09f69f8055ba39a2ef9bf41f
Germán <coccagerman@gmail.com>
2021-02-02 22:19:17 -0300

    Agregado de form para simulador
    1er versión - HTML, resta estilo en CSS y vincular a DOM con JS.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 
-// ----------------------- CONSTRUCCION DE OBJETO -----------------------
+// // ----------------------- CREACION DEL ARRAY -----------------------
 
-function Credito (tipo, monto, plazo) {
-    this.tipo = tipo;
-    this.monto = monto;
-    this.plazo = plazo;
+// var creditoArray = [prompt ("Qué tipo de crédito quiere solicitar?"), parseInt (prompt ("Por qué monto le gustaría solicitar el crédito?")), parseInt (prompt ("En cuántas cuotas le gustaría devolverlo?"))]
 
-    this.simulacion = function () {
-    return monto / plazo; 
-    }
-}
+// // ----------------------- UTILIZACION DE METODO -----------------------
 
-// ----------------------- CREACION DE OBJETO -----------------------
+// alert ("Para obtener un crédito " + creditoArray[0] + " por $" + creditoArray[1] + " con las características que usted requirió, deberá abonar " + creditoArray[2] + " cuotas mensuales de " + creditoArray[1]/creditoArray[2] + ".")
 
-var miCredito = new Credito (prompt ("Qué tipo de crédito quiere solicitar?"), parseInt (prompt ("Por qué monto le gustaría solicitar el crédito?")), parseInt (prompt ("En cuántas cuotas le gustaría devolverlo?")));
+// // ----------------------- IF + PROPIEDADES DE ARRAY -----------------------
 
-// ----------------------- UTILIZACION DE METODO -----------------------
+// if (prompt("Le gustaría pedir otro préstamo?\nIngrese 1 para indicar 'sí'. Ingrese cualquier otro valor para indicar 'no'.") == 1) {
+//     creditoArray.push ( prompt ("Qué tipo de crédito quiere solicitar?"), parseInt (prompt ("Por qué monto le gustaría solicitar el crédito?")), parseInt (prompt ("En cuántas cuotas le gustaría devolverlo?")) )
 
-alert ("Para obtener un crédito " + miCredito.tipo + " por " + miCredito.monto + " con las características que usted requirió, deberá abonar " + miCredito.plazo + " cuotas mensuales de " + miCredito.simulacion() + ".")
+//     alert ("Para obtener un crédito " + creditoArray[3] + " por $" + creditoArray[4] + " con las características que usted requirió, deberá abonar " + creditoArray[5] + " cuotas mensuales de $" + creditoArray[4]/creditoArray[5] + ".")
+    
+//     var creditoAnterior = creditoArray.slice (0, 3)
+
+//     alert ("Recuerde que previamente usted solicitó un crédito con las siguientes características:\nTipo - " + creditoAnterior[0] + "\nMonto - $" + creditoAnterior[1] + "\nPlazo - " + creditoAnterior[2] + "meses.")
+// }
+
+
