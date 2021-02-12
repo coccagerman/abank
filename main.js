dfb84bd02aaa0e0530f6e4ce2eb14b22b789c153
Germán <coccagerman@gmail.com>
2021-02-11 21:35:42 -0300

    Avances en la sección simulador.

--- main.js
+++ main.js
@@ -1,88 +1,88 @@
-let creditsArray = ["PERSONAL", "PRENDARIO", "HIPOTECARIO", "EMPRESARIAL"]
+// let creditsArray = ["PERSONAL", "PRENDARIO", "HIPOTECARIO", "EMPRESARIAL"]
 
-// ----------------------- PROMPT NOMBRE USUARIO -----------------------
+// // ----------------------- PROMPT NOMBRE USUARIO -----------------------
 
-let userName = prompt ("Ingrese su nombre.")
+// let userName = prompt ("Ingrese su nombre.")
 
-// ----------------------- CTRL NOMBRE USUARIO -----------------------
+// // ----------------------- CTRL NOMBRE USUARIO -----------------------
 
-while (userName == "" || userName == null || (!isNaN(userName))) {
-    userName = prompt ("Ingrese su nombre.")
-}
+// while (userName == "" || userName == null || (!isNaN(userName))) {
+//     userName = prompt ("Ingrese su nombre.")
+// }
 
-// ----------------------- PROMPT TIPO CREDITO -----------------------
+// // ----------------------- PROMPT TIPO CREDITO -----------------------
 
-let creditType = prompt("Ingrese tipo de crédito a cotizar.", creditsArray[0] + " | " + creditsArray[1] + " | " + creditsArray[2] + " | " + creditsArray[3]).toUpperCase()
+// let creditType = prompt("Ingrese tipo de crédito a cotizar.", creditsArray[0] + " | " + creditsArray[1] + " | " + creditsArray[2] + " | " + creditsArray[3]).toUpperCase()
 
-// ----------------------- CTRL TIPO CREDITO -----------------------
+// // ----------------------- CTRL TIPO CREDITO -----------------------
 
-let creditAllowed = false
-for (let i = 0; i < creditsArray.length; i++) {
-    if (creditType == creditsArray[i]) {
-        creditAllowed = true
-        break
-    }
-}
+// let creditAllowed = false
+// for (let i = 0; i < creditsArray.length; i++) {
+//     if (creditType == creditsArray[i]) {
+//         creditAllowed = true
+//         break
+//     }
+// }
 
-while (creditType == "" || creditType == null || creditAllowed == false) {
-    creditType = prompt ("Ingrese tipo de crédito a cotizar.", "PERSONAL | PRENDARIO | HIPOTECARIO | EMPRESARIAL")
-}
+// while (creditType == "" || creditType == null || creditAllowed == false) {
+//     creditType = prompt ("Ingrese tipo de crédito a cotizar.", "PERSONAL | PRENDARIO | HIPOTECARIO | EMPRESARIAL")
+// }
 
-// ----------------------- PROMPT MONTO CREDITO -----------------------
+// // ----------------------- PROMPT MONTO CREDITO -----------------------
 
-let creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
+// let creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
 
-// ----------------------- CTRL MONTO CREDITO -----------------------
+// // ----------------------- CTRL MONTO CREDITO -----------------------
 
-let amountCtrl
+// let amountCtrl
 
-if (creditAmount < 100000 || creditAmount > 100000000) {
-    amountCtrl = false
-} else {
-    amountCtrl = true
-}
+// if (creditAmount < 100000 || creditAmount > 100000000) {
+//     amountCtrl = false
+// } else {
+//     amountCtrl = true
+// }
 
-while (creditAmount == "" || creditAmount == null || (isNaN(creditAmount)) || amountCtrl == false) {
-    creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
-}
+// while (creditAmount == "" || creditAmount == null || (isNaN(creditAmount)) || amountCtrl == false) {
+//     creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
+// }
 
-// ----------------------- PROMPT CUOTAS -----------------------
+// // ----------------------- PROMPT CUOTAS -----------------------
 
-let creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
+// let creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
 
-// ----------------------- CTRL CUOTAS -----------------------
+// // ----------------------- CTRL CUOTAS -----------------------
 
-let installmentsCtrl
+// let installmentsCtrl
 
-if (creditInstallments < 12 || creditInstallments > 60) {
-    installmentsCtrl = false
-} else {
-    installmentsCtrl = true
-}
+// if (creditInstallments < 12 || creditInstallments > 60) {
+//     installmentsCtrl = false
+// } else {
+//     installmentsCtrl = true
+// }
 
-while (creditInstallments == "" || creditInstallments == null || (isNaN(creditInstallments)) || installmentsCtrl == false) {
-    creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
-}
+// while (creditInstallments == "" || creditInstallments == null || (isNaN(creditInstallments)) || installmentsCtrl == false) {
+//     creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
+// }
 
-// ----------------------- CALCULO DE CREDITO -----------------------
+// // ----------------------- CALCULO DE CREDITO -----------------------
 
-let interestRate
+// let interestRate
 
-if (creditType == "PERSONAL") {
-    interestRate = personalCredit.interestRate
-} else if (creditType == "PRENDARIO") {
-    interestRate = pledgeCredit.interestRate
-} else if (creditType == "HIPOTECARIO") {
-    interestRate = mortgageCredit.interestRate
-} else {
-    interestRate = businessCredit.interestRate
-}
+// if (creditType == "PERSONAL") {
+//     interestRate = personalCredit.interestRate
+// } else if (creditType == "PRENDARIO") {
+//     interestRate = pledgeCredit.interestRate
+// } else if (creditType == "HIPOTECARIO") {
+//     interestRate = mortgageCredit.interestRate
+// } else {
+//     interestRate = businessCredit.interestRate
+// }
 
-// ----------------------- PRINT -----------------------
+// // ----------------------- PRINT -----------------------
 
-let elemento = document.createElement ("p");
-elemento.setAttribute("class", "desafioDOM")
-let texto = document.createTextNode ("Hola " + userName+ "!\nPara solicitar el crédito " + creditType + " por $" + creditAmount + " con las características que detallaste, deberás pagar " + creditInstallments + " cuotas de $" + calculateCredit(creditAmount, creditInstallments, interestRate) + ".");
+// let elemento = document.createElement ("p");
+// elemento.setAttribute("class", "desafioDOM")
+// let texto = document.createTextNode ("Hola " + userName+ "!\nPara solicitar el crédito " + creditType + " por $" + creditAmount + " con las características que detallaste, deberás pagar " + creditInstallments + " cuotas de $" + calculateCredit(creditAmount, creditInstallments, interestRate) + ".");
 
-elemento.appendChild(texto);
-document.getElementById("calculateCredit").appendChild(elemento);
+// elemento.appendChild(texto);
+// document.getElementById("calculateCredit").appendChild(elemento);
