e1c312bf9b243e55ca1a6c8e4a12ba7ff3fbc343
Germán <coccagerman@gmail.com>
2021-02-15 01:25:57 -0300

    avances

--- functions.js
+++ functions.js
@@ -1,7 +1,67 @@
-// function calculateCredit (creditAmount, creditInstallments, interestRate) {
-//     return parseInt((creditAmount * interestRate) / creditInstallments)
-// }
+// // ----------------------- CREDITOS - SLIDERS ----------------------- Ocultar la sección actual y mostrar la siguiente a partir de onclick (como slider) / Se puede optimizar con una única función que recorra un array de secciones a medida que se aprietan los botones.
+
+function switchCreditSlide (destination) {
+    // Tomar sección vigente y darle class hiddenElement.
+    document.getElementsByClassName('activeElement')[0].setAttribute("class", "hiddenElement");
+    // Leer href del botón que se apriete, y a la sección con el mismo ID darle class activeElement // La selección debiese ser cuando el botón se clickea
+    document.getElementById(document.getElementById('btn-credit_personal').getAttribute('href').replace('#','')).setAttribute("class", "activeElement");
+}
+
+// // ----------------------- SIMULADOR -----------------------
+// // ----------------------- SIMULADOR - SLIDERS ----------------------- Ocultar la sección actual y mostrar la siguiente a partir de onclick (como slider) / Se puede optimizar con una única función que recorra un array de secciones a medida que se aprieta el botón de avanzar. La función de volver a simular debiese ser única, ocultando la sección que está activa y activando la sección simulador. 
 
 function calcularCuotas () {
     document.getElementById("simulator-form").setAttribute("class", "hiddenElement");
+    document.getElementById("simulator-form-response").setAttribute("class", "activeElement");
 }
+
+function solicitarCredito () {
+    document.getElementById("simulator-form-response").setAttribute("class", "hiddenElement");
+    document.getElementById("contact-form").setAttribute("class", "activeElement");
+}
+
+function simularDeNuevo_from_simulatorResponse () {
+    document.getElementById("simulator-form-response").setAttribute("class", "hiddenElement");
+    document.getElementById("simulator-form").setAttribute("class", "activeElement");
+}
+
+function enviarFormContacto () {
+    document.getElementById("contact-form").setAttribute("class", "hiddenElement");
+    document.getElementById("contact-form-response").setAttribute("class", "activeElement");
+}
+
+function simularDeNuevo_from_contactForm () {
+    document.getElementById("contact-form").setAttribute("class", "hiddenElement");
+    document.getElementById("simulator-form").setAttribute("class", "activeElement");
+}
+
+function simularDeNuevo_from_contactFormResponse () {
+    document.getElementById("contact-form-response").setAttribute("class", "hiddenElement");
+    document.getElementById("simulator-form").setAttribute("class", "activeElement");
+}
+
+
+// // ----------------------- SIMULADOR - FORM - Modificar cantidad de cuotas según tipo de crédito seleccionado -----------------------
+
+// function populateSimulatorForm (tipoCredito) {
+
+//     if (tipoCredito == "Personal") {
+// 		var arrayCuotas = ["6|6 cuotas","12|12 cuotas","18|18 cuotas"];
+
+// 	} else if (tipoCredito == "Prendario") {
+// 		var arrayCuotas = ["12|12 cuotas","24|24 cuotas","36|36 cuotas"];
+
+// 	} else if (tipoCredito == "Hipotecario") {
+// 		var arrayCuotas = ["60|60 cuotas","120|120 cuotas","180|180 cuotas"];
+
+// 	} else {
+// 		var arrayCuotas = ["6|6 cuotas","24|24 cuotas","48|48 cuotas", "60|60 cuotas"];
+// 	}
+
+//     for (var option in arrayCuotas){
+// 		var pair = arrayCuotas[option].split("|");
+// 		var newOption = document.createElement("option");
+// 		newOption.value = pair[0];
+// 		newOption.innerHTML = pair[1];
+// 	}
+// }
--- main.js
+++ main.js
@@ -1,120 +1,63 @@
-// let creditsArray = ["PERSONAL", "PRENDARIO", "HIPOTECARIO", "EMPRESARIAL"]
-
-// // ----------------------- PROMPT NOMBRE USUARIO -----------------------
-
-// let userName = prompt ("Ingrese su nombre.")
-
-// // ----------------------- CTRL NOMBRE USUARIO -----------------------
-
-// while (userName == "" || userName == null || (!isNaN(userName))) {
-//     userName = prompt ("Ingrese su nombre.")
-// }
-
-// // ----------------------- PROMPT TIPO CREDITO -----------------------
-
-// let creditType = prompt("Ingrese tipo de crédito a cotizar.", creditsArray[0] + " | " + creditsArray[1] + " | " + creditsArray[2] + " | " + creditsArray[3]).toUpperCase()
-
-// // ----------------------- CTRL TIPO CREDITO -----------------------
-
-// let creditAllowed = false
-// for (let i = 0; i < creditsArray.length; i++) {
-//     if (creditType == creditsArray[i]) {
-//         creditAllowed = true
-//         break
-//     }
-// }
-
-// while (creditType == "" || creditType == null || creditAllowed == false) {
-//     creditType = prompt ("Ingrese tipo de crédito a cotizar.", "PERSONAL | PRENDARIO | HIPOTECARIO | EMPRESARIAL")
-// }
-
-// // ----------------------- PROMPT MONTO CREDITO -----------------------
-
-// let creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
-
-// // ----------------------- CTRL MONTO CREDITO -----------------------
-
-// let amountCtrl
-
-// if (creditAmount < 100000 || creditAmount > 100000000) {
-//     amountCtrl = false
-// } else {
-//     amountCtrl = true
-// }
-
-// while (creditAmount == "" || creditAmount == null || (isNaN(creditAmount)) || amountCtrl == false) {
-//     creditAmount = parseInt(prompt ("Ingrese el monto del crédito que desea solicitar.", "Monto mínimo: $100.000 - Monto máximo: $100.000.000"))
-// }
-
-// // ----------------------- PROMPT CUOTAS -----------------------
-
-// let creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
-
-// // ----------------------- CTRL CUOTAS -----------------------
-
-// let installmentsCtrl
-
-// if (creditInstallments < 12 || creditInstallments > 60) {
-//     installmentsCtrl = false
-// } else {
-//     installmentsCtrl = true
-// }
-
-// while (creditInstallments == "" || creditInstallments == null || (isNaN(creditInstallments)) || installmentsCtrl == false) {
-//     creditInstallments = parseInt(prompt("Ingrese la cantidad de cuotas en las que desea pagar su crédito.", "Cantidad mínima: 12 - Cantidad máxima: 60"))
-// }
-
-// // ----------------------- CALCULO DE CREDITO -----------------------
-
-// let interestRate
-
-// if (creditType == "PERSONAL") {
-//     interestRate = personalCredit.interestRate
-// } else if (creditType == "PRENDARIO") {
-//     interestRate = pledgeCredit.interestRate
-// } else if (creditType == "HIPOTECARIO") {
-//     interestRate = mortgageCredit.interestRate
-// } else {
-//     interestRate = businessCredit.interestRate
-// }
-
-// // ----------------------- PRINT -----------------------
-
-// let elemento = document.createElement ("p");
-// elemento.setAttribute("class", "desafioDOM")
-// let texto = document.createTextNode ("Hola " + userName+ "!\nPara solicitar el crédito " + creditType + " por $" + creditAmount + " con las características que detallaste, deberás pagar " + creditInstallments + " cuotas de $" + calculateCredit(creditAmount, creditInstallments, interestRate) + ".");
-
-// elemento.appendChild(texto);
-// document.getElementById("calculateCredit").appendChild(elemento);
+console.log(document.getElementById('btn-credit_personal').getAttribute('href').replace('#',''));
 
 
-// // ----------------------- SIMULADOR -----------------------
-// // ----------------------- SIMULADOR - FORM -----------------------
+// ----------------------- CREDITOS ----------------------- preventDefault de botones (evita que resetee página luego del click) / Se debe optimizar juntando todos los botones en una array y ejecutando la función sobre el array una sola vez (Unificarl todos los botones por clase en un mismo array)
 
+console.log(document.getElementsByClassName('activeElement')[0])
 
-// function populateSimulatorForm (tipoCredito) {
+document.getElementById("btn-credit_personal").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-credit_prendario").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-credit_hipotecario").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-credit_empresarial").addEventListener('click', function (e) {
+    e.preventDefault();
+})
 
-//     if (tipoCredito == "Personal") {
-// 		var arrayCuotas = ["6|6 cuotas","12|12 cuotas","18|18 cuotas"];
+// ----------------------- CREDITOS ----------------------- Ocultar la sección actual y mostrar la siguiente a partir del onclick (como slider)
 
-// 	} else if (tipoCredito == "Prendario") {
-// 		var arrayCuotas = ["12|12 cuotas","24|24 cuotas","36|36 cuotas"];
+// ----------------------- SIMULADOR -----------------------
+// ----------------------- SIMULADOR - FORM ----------------------- preventDefault de botones del form (evita que resetee página luego del click) / Se debe optimizar juntando todos los botones en una array y ejecutando la función sobre el array una sola vez (Unificarl todos los botones por clase en un mismo array)
 
-// 	} else if (tipoCredito == "Hipotecario") {
-// 		var arrayCuotas = ["60|60 cuotas","120|120 cuotas","180|180 cuotas"];
+document.getElementById("btn-calcularCuotas").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-contact-form").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-volverSimular_from_simulatorResponse").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-enviar-contact-form").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-volverSimular_from_contactForm").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-creditHistory").addEventListener('click', function (e) {
+    e.preventDefault();
+})
+document.getElementById("btn-volverSimular_from_contactFormResponse").addEventListener('click', function (e) {
+    e.preventDefault();
+})
 
-// 	} else {
-// 		var arrayCuotas = ["6|6 cuotas","24|24 cuotas","48|48 cuotas", "60|60 cuotas"];
-// 	}
-
-//     for (var option in arrayCuotas){
-// 		var pair = arrayCuotas[option].split("|");
-// 		var newOption = document.createElement("option");
-// 		newOption.value = pair[0];
-// 		newOption.innerHTML = pair[1];
-// 	}
-
-//     console.log(newOption)
-// }
+// ----------------------- SIMULADOR - FORM ----------------------- Ocultar la sección actual y mostrar la siguiente a partir del onclick (como slider)
 
 document.getElementById("btn-calcularCuotas").onclick = calcularCuotas;
+
+document.getElementById("btn-contact-form").onclick = solicitarCredito;
+
+document.getElementById("btn-volverSimular_from_simulatorResponse").onclick = simularDeNuevo_from_simulatorResponse;
+
+document.getElementById("btn-enviar-contact-form").onclick = enviarFormContacto;
+
+document.getElementById("btn-volverSimular_from_contactForm").onclick = simularDeNuevo_from_contactForm;
+
+document.getElementById("btn-volverSimular_from_contactFormResponse").onclick = simularDeNuevo_from_contactFormResponse;
+
+
+// ----------------------- SIMULADOR - FORM - Modificar cantidad de cuotas según tipo de crédito seleccionado -----------------------
