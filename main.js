03e35f45916385a937adf4a14e5a0e4a5d6fedf6
Germán <coccagerman@gmail.com>
2021-02-26 18:58:51 -0300

    Improvements on main.js
    Improvements on main.js, rewritting code in vainilla JS to JQuery.

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ $(window).on("load", function() {
 $(".preloader_wrapper").fadeOut(2000);
 })
 
-
 // ----------------------- NAVBAR ----------------------- Mobile navbar animated button
 
 let hamburguer = 0;
@@ -13,81 +12,43 @@ $('.hamburguer-icon').click(displayMobileMenu);
 
 $('#menu ul li').click(hideMobileMenu);
 
-// ----------------------- CREDITS ----------------------- preventDefault de botones (evita que resetee página luego del click) / Se debe optimizar juntando todos los botones en una array y ejecutando la función sobre el array una sola vez (Unificarl todos los botones por clase en un mismo array)
+// ----------------------- CREDITS & SIMUALTOR ----------------------- preventDefault de botones (evita que resetee página luego del click) / Eficientizar unificando todos los botones en una clase .preventDefault
 
-document.getElementById("btn-credit_personal").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-credit_pledge").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-credit_mortgage").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-credit_business").addEventListener('click', function (e) {
+$("#btn-credit_personal, #btn-credit_pledge, #btn-credit_mortgage, #btn-credit_business, #btn-calcularCuotas, #btn-contact-form, #btn-volverSimular_from_simulatorResponse, #btn-enviar-contact-form, #btn-enviar-contact-form, #btn-volverSimular_from_contactForm, #btn-creditHistory, #btn-volverSimular_from_contactFormResponse, #btn-volverSimular_from_creditHistory").on('click', function (e) {
     e.preventDefault();
 })
 
 // ----------------------- CREDITS ----------------------- Ocultar la sección actual y mostrar la siguiente a partir del onclick (como slider)
 
-document.getElementById("btn-credit_personal").onclick = slidePersonalCredit;
+$("#btn-credit_personal").on('click', slidePersonalCredit)
 
-document.getElementById("btn-credit_pledge").onclick = slidePledgeCredit;
+$("#btn-credit_pledge").on('click', slidePledgeCredit)
 
-document.getElementById("btn-credit_mortgage").onclick = slideMortgageCredit;
+$("#btn-credit_mortgage").on('click', slideMortgageCredit)
 
-document.getElementById("btn-credit_business").onclick = slideBusinessCredit;
+$("#btn-credit_business").on('click', slideBusinessCredit)
 
 // ----------------------- SIMULATOR -----------------------
-// ----------------------- SIMULATOR - FORM ----------------------- preventDefault de botones del form (evita que resetee página luego del click) / Se debe optimizar juntando todos los botones en una array y ejecutando la función sobre el array una sola vez (Unificar todos los botones por clase en un mismo array)
-
-document.getElementById("btn-calcularCuotas").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-contact-form").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-volverSimular_from_simulatorResponse").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-enviar-contact-form").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-volverSimular_from_contactForm").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-creditHistory").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-document.getElementById("btn-volverSimular_from_contactFormResponse").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-
-document.getElementById("btn-volverSimular_from_creditHistory").addEventListener('click', function (e) {
-    e.preventDefault();
-})
-
 // ----------------------- SIMULATOR - FORM ----------------------- Ocultar la sección actual y mostrar la siguiente a partir del onclick (como slider)
 
-document.getElementById("btn-calcularCuotas").onclick = calcularCuotas;
+$("#btn-calcularCuotas").on('click', calcularCuotas);
 
-document.getElementById("btn-contact-form").onclick = solicitarCredito;
+$("#btn-contact-form").on('click', solicitarCredito);
 
-document.getElementById("btn-volverSimular_from_simulatorResponse").onclick = simularDeNuevo_from_simulatorResponse;
+$("#btn-volverSimular_from_simulatorResponse").on('click', simularDeNuevo_from_simulatorResponse);
 
-document.getElementById("btn-enviar-contact-form").onclick = enviarFormContacto;
+$("#btn-enviar-contact-form").on('click', enviarFormContacto);
 
-document.getElementById("btn-volverSimular_from_contactForm").onclick = simularDeNuevo_from_contactForm;
+$("#btn-volverSimular_from_contactForm").on('click', simularDeNuevo_from_contactForm);
 
-document.getElementById("btn-creditHistory").onclick = verHistorialCreditos;
+$("#btn-creditHistory").on('click', verHistorialCreditos);
 
-document.getElementById("btn-volverSimular_from_contactFormResponse").onclick = simularDeNuevo_from_contactFormResponse;
+$("#btn-volverSimular_from_contactFormResponse").on('click', simularDeNuevo_from_contactFormResponse);
 
-document.getElementById("btn-volverSimular_from_creditHistory").onclick = simularDeNuevo_from_creditHistory;
-
+$("#btn-volverSimular_from_creditHistory").on('click', simularDeNuevo_from_creditHistory);
 
 // ----------------------- SIMULATOR - FORM ------------------------ Modificar cantidad de cuotas según tipo de crédito seleccionado y el monto disponible según el nivel de ingresos declarado
 
-document.getElementById("tipoCredito").addEventListener("change", adjustFormtOptions)
-document.getElementById("ingresosNetos").addEventListener("change", adjustFormtOptions)
-document.addEventListener("DOMContentLoaded", adjustFormtOptions) // Para que figuren opciones apenas cargue el sitio, sin que la selección necesariamente cambie
+$("#tipoCredito").on("change", adjustFormtOptions)
+$("#ingresosNetos").on("change", adjustFormtOptions)
+$(document).on("DOMContentLoaded", adjustFormtOptions) // Para que figuren opciones apenas cargue el sitio, sin que la selección necesariamente cambie
