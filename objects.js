374ef1554d041dd5934cfb43ec6bdcf7547fbbfb
Germán <coccagerman@gmail.com>
2021-02-25 22:32:23 -0300

    Minor improvements
    Minor improvements regarding change of classes and functions names and bug fixing.

--- objects.js
+++ objects.js
@@ -10,4 +10,4 @@ function Credit (type, interestRate, minAmount, topAmount, minInstallments, topI
 let personalCredit = new Credit ("PERSONAL", 1.65, 100000, 1000000, 6, 18);
 let pledgeCredit = new Credit ("PRENDARIO", 1.60, 500000, 2000000, 12, 36);
 let mortgageCredit = new Credit ("HIPOTECARIO", 1.55, 10000000, 50000000, 60, 180);
-let businessCredit = new Credit ("EMPRESARIAL", 1.50, 10000000, 10000000, 24, 60);
+let businessCredit = new Credit ("EMPRESARIAL", 1.50, 10000000, 100000000, 24, 60);
